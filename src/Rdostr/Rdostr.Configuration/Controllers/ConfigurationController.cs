701a0ff168ecccfea814ac6b18dc7641c2554253
Daniel Larsen <dalars@microsoft.com>
2019-06-22 22:52:05 +1200

    Config service that returns config to authed user

---
+++ src/Rdostr/Rdostr.Configuration/Controllers/ConfigurationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rdostr.Configuration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            if (User == null || User.Claims == null)
+            {
+                return new UnauthorizedResult();
+            }
+
+            ClaimsCheck(User.Claims);
+
+            //TODO: Get Key from KeyVault
+            var config = new { Foo = "Bar" };
+
+            return new JsonResult(config);
+        }
+
+        private void ClaimsCheck(IEnumerable<Claim> claims)
+        {
+            //TODO: Implement check claims business logic
+            return;
+        }
+    }
+}
---
+++ src/Rdostr/Rdostr.Configuration/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Rdostr.Configuration
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // https://login.microsoftonline.com/tfp/{Configuration["AzureAdB2C:Tenant"]}/{Configuration["AzureAdB2C:Policy"]}/v2.0/
+                    options.Authority = "https://login.microsoftonline.com/tfp/rdostr.onmicrosoft.com/b2c_1_signupsigninv2/v2.0/";
+                    options.Audience = "49df1c13-8de7-46fc-a1d9-7bf0c8b73377";
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = AuthenticationFailed
+                    };
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+
+        private static Task AuthenticationFailed(AuthenticationFailedContext arg)
+        {
+            Trace.TraceError(arg.Exception.Message);
+            return Task.CompletedTask;
+        }
+
+    }
+}
