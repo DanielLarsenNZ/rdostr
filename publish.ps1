4270ca40b0bbe36b0c6fa0c14a9e06596537484c
Daniel Larsen <dalars@microsoft.com>
2019-12-18 21:44:07 +1300

    az acr build �

--- publish.ps1
+++ publish.ps1
@@ -3,6 +3,8 @@ $location = 'australiaeast'
 $loc = 'aue'
 $registry = "helloaks$loc"
 
-az acr build -r $registry https://github.com/DanielLarsenNZ/rdostr.git -f src/Rdostr/Rdostr.Configuration/Dockerfile -t rdostr/configuration:latest
+#az acr build -r $registry -t rdostr/configuration:latest src/Rdostr
 
-az acr build -r $registry https://github.com/DanielLarsenNZ/rdostr.git -f src/Rdostr/Rdostr.Stations/Dockerfile -t rdostr/stations:latest
+az acr build -r $registry https://github.com/DanielLarsenNZ/rdostr.git -f src/Rdostr -t rdostr/configuration:latest
+
+#az acr build -r $registry https://github.com/DanielLarsenNZ/rdostr.git -f src/Rdostr/Rdostr.Stations/Dockerfile -t rdostr/stations:latest
